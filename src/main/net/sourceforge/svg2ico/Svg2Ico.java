40d20273f0d3e9b356173c4a17c67f810a3e108c
Mark Slater <mark.slater@mail.com>
2012-04-06 11:46:28 +0100

    Mark - Fix copyright.

--- src/main/net/sourceforge/svg2ico/Svg2Ico.java
+++ src/main/net/sourceforge/svg2ico/Svg2Ico.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2012 Mark Slater
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package net.sourceforge.svg2ico;
 
 import net.sf.image4j.codec.ico.ICOEncoder;
@@ -13,7 +23,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class Svg2Ico {
+public final class Svg2Ico {
     public void execute() {
         try {
             BufferedImage bufferedImage = loadImage(new File("docs/favicon.svg"), 32, 32);
