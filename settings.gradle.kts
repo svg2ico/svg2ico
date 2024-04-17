9e45618fbe1f109881ae8dc494fc4ad06bb07b49
Mark Slater <mark.slater@mail.com>
2024-04-17 20:29:53 +0100

    Set up toolchain repository so that Gradle can get Java 8.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,4 +8,8 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
+}
+
 rootProject.name = "svg2ico"
